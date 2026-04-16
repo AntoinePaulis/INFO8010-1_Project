18a5b41d10c7528c86d0718d60be7a6e183bdc2f
AntoinePaulis <paulisantoine@gmail.com>
2026-04-16 21:01:21 +0200

    Split change directory

--- project/code/src/player_tracking/split.py
+++ project/code/src/player_tracking/split.py
@@ -3,9 +3,9 @@
 import supervision as sv
 
 ds = sv.DetectionDataset.from_yolo(
-    images_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/train/images",
-    annotations_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/train/labels",
-    data_yaml_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/data.yaml"
+    images_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/train/images",
+    annotations_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/train/labels",
+    data_yaml_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/data.yaml"
 )
 
 train_ds, temp_ds = ds.split(split_ratio=0.7,random_state=42, shuffle=True)
@@ -15,16 +15,16 @@ val_ds, test_ds = temp_ds.split(split_ratio=0.67, random_state=42, shuffle=True)
 print(len(train_ds), len(val_ds), len(test_ds))
 
 train_ds.as_yolo(
-    images_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/train/images",
-    annotations_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/train/labels",
+    images_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/train/images",
+    annotations_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/train/labels",
 )
 
 val_ds.as_yolo(
-    images_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/val/images",
-    annotations_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/val/labels",
+    images_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/val/images",
+    annotations_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/val/labels",
 )
 
 test_ds.as_yolo(
-    images_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/test/images",
-    annotations_directory_path="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/test/labels",
+    images_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/test/images",
+    annotations_directory_path="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/test/labels",
 )
