f87d520e72371c5b2f697fce7febd135974a84c1
AntoinePaulis <paulisantoine@gmail.com>
2026-04-17 14:26:50 +0200

    Add Yolo model

--- project/code/src/player_tracking/model.py
+++ project/code/src/player_tracking/model.py
@@ -1,38 +1,47 @@
 from ultralytics import YOLO
 import wandb
 
+# Now test different YOLO and different config
+
 parameters = {
     "model" : "yolov8n.pt",
-    "eporchs" : 50,
-    "imgsz" : 640,
-    "batch" : 16,
+    "eporchs" : 1,
+    "imgsz" : 320,
+    "batch" : 2,
+    "device" : "cpu"
 }
 
 run = wandb.init(
     entity="uliege-tennis-tracking",
-    project="tennis-tracking",
-    name="yolov8n_test"
+    project="player-tracking",
+    name="yolov8n_test",
     config=parameters
 )
 
-# Charger un modèle YOLOv8 préentraîné
-model = YOLO("yolov8n.pt")  # n=nano, s=small, m=medium, l=large, x=xlarge
+model = YOLO("yolov8n.pt")
 
-# Entraîner
 model.train(
-    data="/scratch/users/andyjalloh/Tennis_Player_Detection.yolov8/data.yaml",
+    data="/scratch/users/andyjalloh/player_tracking/Tennis_Player_Detection.yolov8/data.yaml",
     epochs=parameters["eporchs"],
     imgsz=parameters["imgsz"],
     batch=parameters["batch"],
     project="runs/tennis_player",
-    name="yolov8_exp1"
+    name="yolov8_test",
+    device=parameters["device"]
 )
 
 # Évaluation sur le test set
 metrics = model.val(split="test")
 
+table = wandb.Table(columns=["Metric", "Value"])
+table.add_data("mAP50-95",metrics.box.map)
+table.add_data("mAP50",metrics.box.map50)
+table.add_data("mAP75",metrics.box.map75)
+table.add_data("Precision",metrics.box.mp)
+table.add_data("Recall",metrics.box.mr)
+table.add_data("F1",metrics.box.f1.mean())
+
+# Further add loss if necessary
+
+wandb.log({"Test Metrics": table})
 wandb.finish()
-
-# print the metrics
-
-# add wandb stuf
