aa828a65da21d525979ad488a44c8fb4a5bedc87
AndyJalloh <andy.jalloh@student.uliege.be>
2026-05-05 15:17:11 +0200

    update in config thats user-sensitive for visualization of predictions

--- project/code/src/config.py
+++ project/code/src/config.py
@@ -5,8 +5,10 @@ ENV = os.environ.get("PROJECT_ENV", "local")
 # redirecting towards the alan location for the raw data
 if ENV == "alan":
     DATA_RAW = "/scratch/users/andyjalloh/Dataset" 
+    OUTPUTS_DIR = "/home/users/andyjalloh/andy/INFO8010-1_Project/project/outputs"
 else:
     DATA_RAW = "data/raw"
+    OUTPUTS_DIR = "outputs"
 
 # once the data will be normalized, maybe augmented... through preprocess.py
 DATA_PROCESSED = "/scratch/users/andyjalloh/data_processed" if ENV == "alan" else "data/processed"
